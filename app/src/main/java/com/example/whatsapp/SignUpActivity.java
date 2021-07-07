7fe240202660488bfa50a3c04a63471eddf672db
Muhodari Sage <sagemuho@gmail.com>
2021-07-07 19:23:38 +0200

    added view binding

--- app/src/main/java/com/example/whatsapp/SignUpActivity.java
+++ app/src/main/java/com/example/whatsapp/SignUpActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.whatsapp.databinding.ActivitySignUpBinding;
+
 public class SignUpActivity extends AppCompatActivity {
+    ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
 
         getSupportActionBar().hide();
