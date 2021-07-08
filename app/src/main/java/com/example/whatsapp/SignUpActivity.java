0e8c4760fabaf4bce9c36ceba8b132c4618dc497
Muhodari Sage <sagemuho@gmail.com>
2021-07-08 03:08:10 +0200

    setting up realtime fire base database

--- app/src/main/java/com/example/whatsapp/SignUpActivity.java
+++ app/src/main/java/com/example/whatsapp/SignUpActivity.java
@@ -3,11 +3,17 @@ package com.example.whatsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.whatsapp.databinding.ActivitySignUpBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
     ActivitySignUpBinding binding;
+    private FirebaseAuth auth;
+    FirebaseDatabase  database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,18 @@ public class SignUpActivity extends AppCompatActivity {
 
 
         getSupportActionBar().hide();
+        auth= FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.createUserWithEmailAndPassword
+                        (binding.etEmail.getText().toString(),binding.etPassword.getText().toString());
+
+
+            }
+        });
+
     }
 }
