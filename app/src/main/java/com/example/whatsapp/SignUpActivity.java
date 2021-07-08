c5bf5f886dcaea2545873da8166509ba5195cd86
Muhodari Sage <sagemuho@gmail.com>
2021-07-08 03:52:53 +0200

    able to show sinup control on Phone

--- app/src/main/java/com/example/whatsapp/SignUpActivity.java
+++ app/src/main/java/com/example/whatsapp/SignUpActivity.java
@@ -1,11 +1,16 @@
 package com.example.whatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.whatsapp.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -30,7 +35,26 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 auth.createUserWithEmailAndPassword
-                        (binding.etEmail.getText().toString(),binding.etPassword.getText().toString());
+                        (binding.etEmail.getText().toString(),binding.etPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                   if(task.isSuccessful()){
+                       Toast.makeText(SignUpActivity.this, "User created Successfully", Toast.LENGTH_SHORT).show();
+                   }
+                   else {
+                       Toast.makeText(SignUpActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                   }
+
+
+
+
+
+
+                    }
+                });
 
 
             }
