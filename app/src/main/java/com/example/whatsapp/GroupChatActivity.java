6559cfea86793cc9348187f5abf59c7d8edeb7e3
Muhodari Sage <sagemuho@gmail.com>
2021-07-12 23:44:33 +0200

    added send message in group chat

--- app/src/main/java/com/example/whatsapp/GroupChatActivity.java
+++ app/src/main/java/com/example/whatsapp/GroupChatActivity.java
@@ -1,10 +1,21 @@
 package com.example.whatsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.whatsapp.Adapters.ChatAdapter;
+import com.example.whatsapp.Models.MessageModel;
 import com.example.whatsapp.databinding.ActivityGroupChatBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class GroupChatActivity extends AppCompatActivity {
 
@@ -16,10 +27,55 @@ public class GroupChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityGroupChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GroupChatActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final ArrayList<MessageModel> messageModels = new ArrayList<>();
+
+        final String senderId = FirebaseAuth.getInstance().getUid();
+        binding.userName.setText("Friends Group");
+
+        final ChatAdapter adapter = new ChatAdapter(messageModels, this);
+        binding.chatRecyclerView.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.chatRecyclerView.setLayoutManager(layoutManager);
+
+//        send message in group chat
+
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String message = binding.etMessage.getText().toString();
+                final MessageModel model = new MessageModel(senderId,message);
+                model.setTimestamp(new Date().getTime());
+                binding.etMessage.setText("");
+
+                database.getReference().child("Group Chat")
+                        .push()
+                        .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                    }
+                });
+
+
+
+            }
+        });
 
 
     }
-
-
 
 }
