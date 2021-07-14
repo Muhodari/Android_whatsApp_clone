7d07883b52b590e6a8498df2247b9e19791cbb13
Muhodari Sage <sagemuho@gmail.com>
2021-07-14 10:36:47 +0200

    updated user profile

--- app/src/main/java/com/example/whatsapp/SettingsActivity.java
+++ app/src/main/java/com/example/whatsapp/SettingsActivity.java
@@ -1,18 +1,120 @@
 package com.example.whatsapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.whatsapp.Models.Users;
+import com.example.whatsapp.databinding.ActivitySettingsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    ActivitySettingsBinding binding;
+    FirebaseStorage storage;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-
+        binding=ActivitySettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportActionBar().hide();
 
+        storage =FirebaseStorage.getInstance();
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+
+
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SettingsActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Users users=snapshot.getValue(Users.class);
+                        Picasso.get()
+                                .load(users.getProfilepic())
+                                .placeholder(R.drawable.ic_user)
+                                .into(binding.profileImage);
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        binding.plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent();
+                intent.setAction(intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,33);
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(data.getData() !=null){
+            Uri isFile = data.getData();
+            binding.profileImage.setImageURI(isFile);
+
+            final StorageReference reference = storage.getReference().child("profile_picture")
+                    .child(FirebaseAuth.getInstance().getUid());
+            reference.putFile(isFile).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid())
+                                    .child("profilepic").setValue(uri.toString());
+                            Toast.makeText(SettingsActivity.this, "profile pic Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            });
+
+
+
+        }
 
     }
 }
